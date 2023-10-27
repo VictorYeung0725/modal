f26d436248951e7cdf0bd37d026e6a14c568c7ed
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-10-27 14:08:53 +0800

    a small modal excercise

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 'use strict';
 
 //select all the need element into variable
-const modal = document.getElementsByClassName('modal');
-modal.addEventListener('click', handlClick);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+// modal.addEventListener('click', handlClick);
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
